324074a76e8dac9c72e664f6013c14ea0792eddf
Giorgi Tabatadze <android.giorgi@gmail.com>
2016-12-22 14:59:38 +0400

    Initial

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+$(function(){
+
+    var templates = {
+        progress: "<div class='progress'>\
+        <div class='indeterminate'></div>\
+        </div>"
+    };
+
+    $(".parallax").parallax();
+    $(".carousel.full").carousel({
+        full_width: true,
+        no_wrap: true
+    });
+
+    $(".button-collapse").sideNav();
+
+    $(".scrollSpy").scrollSpy();
+
+    $("#nav-mobile").find("li").find("a").click(function() {
+        $('.button-collapse').sideNav('hide');
+    });
+
+    $("#view-portfolio-item").modal({
+        ready: function(modal, trigger) {
+            $.post("endpoints/portfolio-item.html", {}, function(data) {
+                $(modal).find(".modal-content").html(data);
+                $(modal).find(".carousel").carousel({
+                    full_width: true,
+                    no_wrap: true
+                });
+            });
+        },
+        complete: function(modal) {
+            $(modal).find(".modal-content").html(templates.progress);
+        }
+    });
+
+
+    var floatingScrollBtn = $("#floatingHome");
+
+    var onScroll;
+    $(window).scroll(onScroll = function(e){
+        if(document.body.scrollTop > 300) {
+            floatingScrollBtn.fadeIn("fast");
+        } else {
+            floatingScrollBtn.fadeOut("fast");
+        }
+    });
+    onScroll();
+});
